3d4fc6c856c925047ead754d01ed5d47e3adde0d
Paulo Ribeiro <plribeiro3000@gmail.com>
2023-03-23 16:08:40 -0300

    chore(*): Initial Commit

---
+++ lib/validates_panamanian_ruc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "validates_panamanian_ruc/version"
+
+module ValidatesPanamanianRuc
+  class Error < StandardError; end
+  # Your code goes here...
+end
